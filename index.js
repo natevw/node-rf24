ba845650d4369d11bbf316e96bd487ed4c9afbe8
Nathan Vander Wilt <nate@calftrail.com>
2013-07-29 22:29:30 -0700

    slightly more sophisticated playing around, i.e. http://nodejs.org/api/addons.html#addons_wrapping_c_objects

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var addon = require("./build/Release/rf24");
-console.log(addon.hello()); // 'world'
+// TODO: we should use https://github.com/TooTallNate/node-bindings for this
+// see https://github.com/arunoda/node-usage as an example
+var _rf24 = require("./build/Release/rf24");
+
+
+var obj = new _rf24.Wrapper(10);
+console.log( obj.plusOne() ); // 11
+console.log( obj.plusOne() ); // 12
+console.log( obj.plusOne() ); // 13
--- rf24-wrapper.cc
+++ rf24-wrapper.cc
@@ -3,14 +3,57 @@
 
 using namespace v8;
 
-Handle<Value> Method(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("world"));
+class Wrapper : public node::ObjectWrap {
+ public:
+  static void Init(v8::Handle<v8::Object> exports);
+
+ private:
+  Wrapper();
+  ~Wrapper();
+
+  static v8::Handle<v8::Value> New(const v8::Arguments& args);
+  static v8::Handle<v8::Value> PlusOne(const v8::Arguments& args);
+  double counter_;
+};
+
+Wrapper::Wrapper() {};
+Wrapper::~Wrapper() {};
+
+void Wrapper::Init(Handle<Object> exports) {
+  // Prepare constructor template
+  Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
+  tpl->SetClassName(String::NewSymbol("Wrapper"));
+  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+  // Prototype
+  tpl->PrototypeTemplate()->Set(String::NewSymbol("plusOne"),
+      FunctionTemplate::New(PlusOne)->GetFunction());
+
+  Persistent<Function> constructor = Persistent<Function>::New(tpl->GetFunction());
+  exports->Set(String::NewSymbol("Wrapper"), constructor);
 }
 
+Handle<Value> Wrapper::New(const Arguments& args) {
+  HandleScope scope;
+
+  Wrapper* obj = new Wrapper();
+  obj->counter_ = args[0]->IsUndefined() ? 0 : args[0]->NumberValue();
+  obj->Wrap(args.This());
+
+  return args.This();
+}
+
+Handle<Value> Wrapper::PlusOne(const Arguments& args) {
+  HandleScope scope;
+
+  Wrapper* obj = ObjectWrap::Unwrap<Wrapper>(args.This());
+  obj->counter_ += 1;
+
+  return scope.Close(Number::New(obj->counter_));
+}
+
+
 void init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("hello"),
-      FunctionTemplate::New(Method)->GetFunction());
+    Wrapper::Init(exports);
 }
 
 NODE_MODULE(rf24, init)
